be4d92ee36a6bece69466c19e5a91c0d067ad3f7
Fullstack Developer <delosmusicc@gmail.com>
2020-09-03 00:57:54 -0700

    Updates

---
+++ precache-manifest.988c98e72d1e877032a707a5f8f25ade.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/MyRobots/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "e78de8d29b374f983540",
+    "url": "/MyRobots/static/js/runtime~main.292acb96.js"
+  },
+  {
+    "revision": "ad5954c78e0a37fa668c",
+    "url": "/MyRobots/static/js/main.1c8b2fc0.chunk.js"
+  },
+  {
+    "revision": "61bdc79c98afb03969b1",
+    "url": "/MyRobots/static/js/2.67f05437.chunk.js"
+  },
+  {
+    "revision": "ad5954c78e0a37fa668c",
+    "url": "/MyRobots/static/css/main.be8f051e.chunk.css"
+  },
+  {
+    "revision": "61bdc79c98afb03969b1",
+    "url": "/MyRobots/static/css/2.58e516f0.chunk.css"
+  },
+  {
+    "revision": "d6a1e4a6f8308656b749624945c6fd4e",
+    "url": "/MyRobots/index.html"
+  }
+];
